bfe52773ad1203157d371f93b3f7b983e63a63c6
Abby Anderson <abbyanderson@Abbys-MacBook-Pro.local>
2021-12-22 11:45:49 -0600

    first draft of solution

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,27 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let duplicateFound = false
+  let duplicateValue = -1
+
+  if (arr.length === 0 || arr.length === 1) {
+    return duplicateValue
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        if (arr[i] === arr[j]) {
+          duplicateFound = true
+          duplicateValue = arr[i]
+          break;
+        }
+      }
+    }
+
+    if (duplicateFound) {
+      return duplicateValue
+    }
+  }
 }
 
 if (require.main === module) {
@@ -16,4 +38,20 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+// let boolean = false
+// first loop (i=0; i< array length; i++)
+// second loop (j=0; j< array length; j++)
+//   in this loop, first make sure we're only comparing with values that we're not currrently holding
+//   if (i !== j) {
+//     then, compare array spots i to j
+//     if (array[i] === array[j]) {
+//       change boolean to true
+//       return something? or break
+//     }
+//   }
+//   THEN close first loop
+//   with if (boolean is true) {
+//     return array[i]
+//   }
+
 // And a written explanation of your solution

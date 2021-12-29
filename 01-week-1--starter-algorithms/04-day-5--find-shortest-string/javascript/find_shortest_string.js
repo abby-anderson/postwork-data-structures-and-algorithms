7c11918e111144f3cb2ed5adc917201b13588f7a
Abby Anderson <abbyanderson@Abbys-MacBook-Pro.local>
2021-12-29 11:06:08 -0600

    Completed a possible solution using array.reduce method

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,5 +1,9 @@
 function findShortestString(arr) {
   // type your code here
+
+  let shortestWord = arr.reduce((a,b) => a.length <= b.length? a :  b)
+
+  return shortestWord
 }
 
 if (require.main === module) {
@@ -15,12 +19,30 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dandelion']));
+
+  console.log("");
+
+  console.log("Expecting: 'mai'");
+  console.log("=>", findShortestString(['abby', 'david', 'frances', 'mai']));
+
+  console.log("");
 
   // BENCHMARK HERE
+  const startTime = Date.now()
+  for (let i = 0; i < 1000; i++) {
+    findShortestString(['flower', 'juniper', 'lily', 'dandelion'])
+  }
+  const averageTime = (startTime - Date.now()) / 1000
+  console.log(averageTime)
 }
 
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+// Use arr.reduce method and a ternary to check if a.length is <= b.length -- if true, set shortestWord to a, if false, set shortestWord to b
+// Save the product in a shortestWord variable
+// after working through the whole array, return shortestWord
+
 // And a written explanation of your solution
+// We're using the array.reduce method on the array that's passed in when the function is called. We look through each word in the array comparing them to each other. If the first is less than or equal to the second, we return the first word -- if not, we return the second. We store that return in a variable called shortestWord. Once the reduce method has compared all the words, we return the shortestWord.
